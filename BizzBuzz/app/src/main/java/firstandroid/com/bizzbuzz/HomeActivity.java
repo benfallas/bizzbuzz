a7690aaeb7e318d1661ababf4bcfd1c51af7682e
FernandoCaudillo10 <caudillojavier@gmail.com>
2017-08-05 15:15:54 -0700

    Added Rules button and RulesActivity tranfer functionality

--- BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/HomeActivity.java
+++ BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/HomeActivity.java
@@ -1,17 +1,31 @@
 package firstandroid.com.bizzbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @BindView(R.id.rules_button) Button rulesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.rules_button)
+    protected void onRulesButtonClicked(){
+        Intent intent = new Intent(this, RulesActivity.class);
+        startActivity(intent);
+    }
 
 }

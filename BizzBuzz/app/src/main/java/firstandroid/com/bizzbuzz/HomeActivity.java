4c54d0065fe7ec13910b5ad9f0c7f2d49fb31d58
Juhi Park <juhipark@student.hartnell.edu>
2017-08-05 15:09:12 -0700

    Adding Play button and links it to new activity named, PlayActivity

--- BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/HomeActivity.java
+++ BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/HomeActivity.java
@@ -1,17 +1,32 @@
 package firstandroid.com.bizzbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @BindView(R.id.play_button) Button play_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.play_button)
+    public void onPlayButtonClicked() {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+
+
+    }
 
 }

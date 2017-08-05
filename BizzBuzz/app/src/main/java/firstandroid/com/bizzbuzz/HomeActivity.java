30050dd2e9df5a3b43a64c86d6ee8da089e16ef9
Juhi Park <juhipark@student.hartnell.edu>
2017-08-05 14:56:41 -0700

    Added butter knife library to gradle

--- BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/HomeActivity.java
+++ BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/HomeActivity.java
@@ -2,12 +2,16 @@ package firstandroid.com.bizzbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+
 }

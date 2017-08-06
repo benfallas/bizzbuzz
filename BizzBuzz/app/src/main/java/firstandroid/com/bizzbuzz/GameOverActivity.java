883ae244e81ef812a6c9072690baeb197f6ed125
Juhi Park <juhipark@student.hartnell.edu>
2017-08-05 18:20:10 -0700

    Adding a disappointed emoji in the Game Over screen

--- BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/GameOverActivity.java
+++ BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/GameOverActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,6 +12,8 @@ import butterknife.OnClick;
 
 public class GameOverActivity extends AppCompatActivity {
     @BindView(R.id.home_button) Button home_button;
+    @BindView(R.id.disappointed_emoji) ImageView disappointed_emoji;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class GameOverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_over);
 
         ButterKnife.bind(this);
+
     }
 
     @OnClick(R.id.home_button)

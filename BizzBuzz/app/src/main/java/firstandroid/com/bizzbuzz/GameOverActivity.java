b08f12b8aa167a7e4eedc3c4e4e3cb4e9e7dc3f8
Juhi Park <juhipark@student.hartnell.edu>
2017-08-05 17:50:39 -0700

    Fixing stacked home acitivity error

--- BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/GameOverActivity.java
+++ BizzBuzz/app/src/main/java/firstandroid/com/bizzbuzz/GameOverActivity.java
@@ -1,13 +1,25 @@
 package firstandroid.com.bizzbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GameOverActivity extends AppCompatActivity {
+    @BindView(R.id.home_button) Button home_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.home_button)
+    public void backToHome() { onBackPressed(); }
 }
